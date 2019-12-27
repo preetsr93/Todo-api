a4d01c683c3a8e4a64de6801c41b0d1a8292c70a
Preethi Sri <preetsr93@gmail.com>
2019-12-27 15:07:00 +0530

    Add ?completed filter

--- server.js
+++ server.js
@@ -11,7 +11,16 @@ app.use(bodyParser.json());
 
 //GET /todos
 app.get('/todos', function (req, res){
-    res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos = todos;
+
+    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+        filteredTodos = _.where(filteredTodos, {completed: true});
+    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+        filteredTodos = _.where(filteredTodos, {completed: false});
+    }
+    
+    res.json(filteredTodos);
 });
 
 //GET /todos/:id
