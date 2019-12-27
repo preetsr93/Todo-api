b414d3f818d0c29545b4154dc3d279517250ae38
Preethi Sri <preetsr93@gmail.com>
2019-12-27 15:07:31 +0530

    Add variable

---
+++ variable.js
@@ -0,0 +1,28 @@
+// var person = {
+//     name : "Preethi",
+//     age : 26  
+// };
+
+// function updatePerson (obj) {
+//     obj.age = 27;
+
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//Array example
+
+var arr = [50, 37];
+
+function updateArr (array) {
+    array = [24];
+}
+
+function updateArr1(array) {
+    array.push(24);
+    debugger;
+}
+
+updateArr1(arr);
+console.log(arr);
